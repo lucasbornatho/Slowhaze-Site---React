c085d93ea466c84521e62994f2151d80e430981b
Lucas Bornatho <lucasbornatho@outlook.com>
2025-02-28 17:09:47 -0300

    alteracoes basicas no cabecalho e criacao da pasta pages

--- slowhaze-site/src/App.tsx
+++ slowhaze-site/src/App.tsx
@@ -1,14 +1,14 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Header from './components/header'
-import NotFound from './components/notfound'
+import NotFound from './pages/notfound'
 
 function App() {
 
   return (
     <Routes>
       <Route path='*' element= {<NotFound />} />
-      <Route path='/' element = {<Header/>} />
+      <Route path='/'  element = {<Header/>} />
     </Routes>
   )
 }
