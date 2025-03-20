3f9b5b5c6558571c5358593ca7392b523be25421
Lucas Bornatho Pires <lucasbornatho@outlook.com>
2025-03-20 17:12:51 -0300

    iniciando a implementacao de um carousel na home, para futura mostra dos albuns da banda

---
+++ slowhaze-site/src/components/carouselhome/index.tsx
@@ -0,0 +1,46 @@
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css'
+
+const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 1,
+      slidesToSlide: 1 
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 1,
+      slidesToSlide: 1 
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1
+    }
+  };
+
+function CarouselHome () {
+    return (
+        <Carousel   swipeable={true}
+        draggable={true}
+        showDots={true}
+        responsive={responsive}
+        ssr={true}
+        infinite={true}
+        autoPlaySpeed={1000}
+        keyBoardControl={true}
+        customTransition="all .5"
+        transitionDuration={500}
+        containerClass="carousel-container"
+        removeArrowOnDeviceType={["tablet", "mobile"]}
+        dotListClass="custom-dot-list-style"
+        itemClass="carousel-item-padding-40-px">
+            <img src='/slowhaze-logo.png' alt='slowhazelogo'></img>
+            <img src='/slowhaze-logo.png' alt='slowhazelogo'></img>
+            <img src='/slowhaze-logo.png' alt='slowhazelogo'></img>
+            <img src='/slowhaze-logo.png' alt='slowhazelogo'></img>
+        </Carousel>
+    )
+}
+
+export default CarouselHome
--- slowhaze-site/src/pages/home.tsx
+++ slowhaze-site/src/pages/home.tsx
@@ -1,10 +1,12 @@
 import Header from "../components/header";
 import Footer from "../components/footer";
+import CarouselHome from "../components/carouselhome";
 
 function Home() {
   return (
     <div>
         <Header />
+        <CarouselHome />
         <Footer />
     </div>
   );
