e442cb70010e47f07444e0e8b71b47fdc1246d6d
Lucas Bornatho Pires <lucasbornatho@yahoo.com.br>
2025-03-22 13:18:16 -0300

    att vite.config.ts

--- slowhaze-site/vite.config.ts
+++ slowhaze-site/vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // Use '/' para desenvolvimento
+  base: '/', // Caminho base para desenvolvimento
   plugins: [react()],
-  build: {
-    outDir: 'dist'
+  server: {
+    open: true, // Abre o navegador automaticamente
   }
 })
